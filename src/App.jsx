03307721030db3b171d61b1cd607675e6ff39bff
Trevor <nyigeitrevor@gmail.com>
2025-04-09 14:26:50 +0300

    lab complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { Profile } from "./components/Profile";
+
 function App() {
-	return (
-		<div className="min-h-screen bg-gray-50 text-gray-800">
-			<main className="p-4"></main>
-		</div>
-	);
+  return (
+    <div className="min-h-screen bg-gray-50 text-gray-800">
+      {/* <main className="p-4"></main> */}
+      <Header title="React components and props" />
+      <div className="container mx-auto p-4">
+        <Profile name="John Free" title="Developer" location="Nairobi,Kenya"/>
+      </div>
+      <Footer message="Do not use AI"/>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-export function Header({ title }) {
+export function Header({title}) {
 	return (
 		<header className="bg-blue-600 text-white p-4 shadow-md">
 			<h1 className="text-xl font-semibold">{title}</h1>
